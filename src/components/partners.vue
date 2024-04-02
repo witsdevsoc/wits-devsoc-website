331a20b01ae968f6b5590daf236a689a3808dc02
Moloko <152928508+sylvester-stud2@users.noreply.github.com>
2024-04-02 12:44:04 +0200

    Create partners.vue

---
+++ src/components/partners.vue
@@ -0,0 +1,830 @@
+<template>
+    <div>
+      
+
+        <section id="partners" class="sectionnn">
+
+<div id="parthead">
+  <h1>Our Partners</h1>
+</div>
+
+<!-- <div class="swiper mySwiper" id="swip">
+  <div class="swiper-wrapper sectionnn">
+
+    <div class="swiper-slide">
+      <div id="bbdpad" class="partcontainer">
+
+
+        <img src="../assets/partners/bbd.jpg" alt="">
+
+
+        <a class="models"
+          href="https://bbdsoftware.com/services/software-development/?utm_source=google+search&utm_medium=cpc&utm_campaign=19744285142&utm_content=144879029925&utm_term=bbd%20software&gclid=CjwKCAjw-7OlBhB8EiwAnoOEk_PwM3BDZZTWA82g4cJEgec16t60oLo3yOdpQVTAsBaGK9U3Sf2-CBoCLqAQAvD_BwE">
+
+
+          <h1>BBD</h1>
+
+
+          <div class="spa">
+            <div class="viewsite">
+              <h4>View site</h4>
+              <div>
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <div class="partcontainer">
+
+
+        <img src="../assets/partners/psyb.jpg" alt="">
+
+
+        <a class="models" href="https://psybergate.co.za/">
+
+
+          <h1>Psybergate</h1>
+
+
+          <div class="spa">
+            <div class="viewsite">
+              <h4>View site</h4>
+              <div>
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <div id="awspad" class="partcontainer">
+
+
+        <img src="../assets/partners/aws.png" alt="">
+
+
+        <a class="models" href="https://aws.amazon.com/">
+
+
+          <h1>AWS</h1>
+
+
+          <div class="spa">
+            <div class="viewsite">
+              <h4>View site</h4>
+              <div>
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+
+
+    <div class="swiper-slide">
+      <div class="partcontainer">
+
+
+        <img src="../assets/partners/ente.png" alt="">
+
+
+        <a class="models" href="https://entelect.co.za/">
+
+
+          <h1>Entelect</h1>
+
+
+          <div class="spa">
+            <div class="viewsite">
+              <h4>View site</h4>
+              <div>
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <div class="partcontainer">
+
+
+        <img src="../assets/partners/retrorab.png" alt="">
+
+
+        <a class="models" href="https://www.retrorabbit.co.za/">
+
+
+          <h1>Retro Rabbit</h1>
+
+
+          <div class="spa">
+            <div class="viewsite">
+              <h4>View site</h4>
+              <div>
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <div id="epiuspad" class="partcontainer">
+
+
+        <img src="../assets/partners/epiuse.png" alt="">
+
+
+        <a class="models" href="https://www.epiuse.com/">
+
+
+          <h1>Epiuse</h1>
+
+
+          <div class="spa">
+            <div class="viewsite">
+              <h4>View site</h4>
+              <div>
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+
+
+
+    <div class="swiper-slide">
+      <div id="bsgimg" class="partcontainer">
+
+
+        <img src="../assets/partners/bsgg.jpg" alt="">
+
+
+        <a class="models" href="https://bsg.co.za/">
+
+
+          <h1>BSG</h1>
+
+
+          <div class="spa">
+            <div class="viewsite">
+              <h4>View site</h4>
+              <div>
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <div id="darpad" class="partcontainer">
+
+
+        <img src="../assets/partners/dariel.jpg" alt="">
+
+
+        <a class="models" href="https://www.dariel.co.za/">
+
+
+          <h1>Dariel</h1>
+
+
+          <div class="spa">
+            <div class="viewsite">
+              <h4>View site</h4>
+              <div>
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <div id="syntpad" class="partcontainer">
+
+
+        <img src="../assets/partners/synth.png" alt="">
+
+
+        <a class="models" href="https://www.synthesis.co.za/">
+
+
+          <h1>Synthesis</h1>
+
+
+          <div class="spa">
+            <div class="viewsite">
+              <h4>View site</h4>
+              <div>
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+
+
+
+    <div class="swiper-slide">
+      <div id="mwrpad" class="partcontainer">
+
+
+        <img src="../assets/partners/mwr.jpg" alt="">
+
+
+        <a class="models" href="https://www.mwrcybersec.com/">
+
+
+          <h1>MWR</h1>
+
+
+          <div class="spa">
+            <div class="viewsite">
+              <h4>View site</h4>
+              <div>
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <div class="partcontainer">
+
+
+        <img src="../assets/partners/bdo.png" alt="">
+
+
+        <a class="models" href="https://www.bdo.co.za/en-za/home">
+
+
+          <h1>BDO</h1>
+
+
+          <div class="spa">
+            <div class="viewsite">
+              <h4>View site</h4>
+              <div>
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <div id="onexpadd" class="partcontainer">
+
+
+        <img src="../assets/partners/onexx.png" alt="">
+
+
+        <a class="models" href="https://www.plusonex.com/">
+
+
+          <h1>+one<sup>x</sup></h1>
+
+
+          <div class="spa">
+            <div class="viewsite">
+              <h4>View site</h4>
+              <div>
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+
+
+
+    <div class="swiper-slide">
+      <div id="moharpad" class="partcontainer">
+
+
+        <img src="../assets/partners/moharaa.png" alt="">
+
+
+        <a class="models" href="https://mohara.co/">
+
+
+          <h1>Mohara</h1>
+
+
+          <div class="spa">
+            <div class="viewsite">
+              <h4>View site</h4>
+              <div>
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <div id="derpad" class="partcontainer">
+
+
+        <img src="../assets/partners/deriv.png" alt="">
+
+
+        <a class="models" href="https://derivco.co.za/">
+
+
+          <h1>Derivico</h1>
+
+
+          <div class="spa">
+            <div class="viewsite">
+              <h4>View site</h4>
+              <div>
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <div class="partcontainer">
+
+
+        <img src="../assets/partners/adapt.png" alt="">
+
+
+        <a class="models" href="https://www.adaptit.com/">
+
+
+          <h1>AdaptIT</h1>
+
+
+          <div class="spa">
+            <div class="viewsite">
+              <h4>View site</h4>
+              <div>
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+
+
+
+    <div class="swiper-slide">
+      <div id="ydrpad" class="partcontainer">
+
+
+        <img id="ydrimg" src="../assets/partners/ydar.jpg" alt="">
+
+
+        <a class="models" href="https://ydragon.io/">
+
+
+          <h1>YDragon</h1>
+
+
+          <div class="spa">
+            <div class="viewsite">
+              <h4>View site</h4>
+              <div>
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <div id="yellpad" class="partcontainer">
+
+
+        <img id="yellbr" src="../assets/partners/yello.png" alt="">
+
+
+        <a class="models" href="https://www.yellow.africa/">
+
+
+          <h1>Yellow</h1>
+
+
+          <div class="spa">
+            <div class="viewsite">
+              <h4>View site</h4>
+              <div>
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <div id="zinpadd" class="partcontainer">
+
+
+        <img src="../assets/partners/zindi.png" alt="">
+
+
+        <a class="models" href="https://zindi.africa/">
+
+
+          <h1>Zindi</h1>
+
+
+          <div class="spa">
+            <div class="viewsite">
+              <h4>View site</h4>
+              <div>
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+
+
+
+  </div>
+  <div class="swiper-button-next" ></div>
+  <div class="swiper-button-prev"></div>
+  <div class="swiper-pagination"></div>
+</div> -->
+
+
+
+
+
+
+
+
+
+<div class="swiper mySwiper" id="swip">
+        <div class="swiper-wrapper sectionnn">
+          <div v-for="(partner, index) in partners" :key="index" class="swiper-slide">
+            <div class="partcontainer">
+              <img :src="partner.image" :alt="partner.name">
+              <a class="models" :href="partner.link">
+                <h1>{{ partner.name }}</h1>
+                <div class="spa">
+                  <div class="viewsite">
+                    <h4>View site</h4>
+                    <div>
+                      <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-pagination"></div>
+      </div>
+
+
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+</template>
+  
+<script>
+  import Swiper from 'swiper/bundle';
+  export default {
+  name: 'partnersSection',
+  data() {
+    return {
+      partners: [
+        { name: 'BBD', image: require('@/assets/partners/bbd.jpg'), link: 'https://bbdsoftware.com/services/software-development/?utm_source=google+search&utm_medium=cpc&utm_campaign=19744285142&utm_content=144879029925&utm_term=bbd%20software&gclid=CjwKCAjw-7OlBhB8EiwAnoOEk_PwM3BDZZTWA82g4cJEgec16t60oLo3yOdpQVTAsBaGK9U3Sf2-CBoCLqAQAvD_BwE' },
+        { name: 'Psybergate', image: require('@/assets/partners/psyb.jpg'), link: 'https://psybergate.co.za/' },
+        { name: 'adapt', image: require('@/assets/partners/adapt.png'), link: 'https://adapt.co.za/' },
+        { name: 'aws', image: require('@/assets/partners/aws.png'), link: 'https://aws.amazon.com/' },
+        { name: 'bdo', image: require('@/assets/partners/bdo.png'), link: 'https://www.bdo.co.za/' },
+        { name: 'bsgg', image: require('@/assets/partners/bsgg.jpg'), link: 'https://bsgg.net/' },
+        { name: 'dariel', image: require('@/assets/partners/dariel.jpg'), link: 'https://www.dariel.co.za/' },
+        { name: 'ente', image: require('@/assets/partners/ente.png'), link: 'https://ente.co.za/' },
+        { name: 'epiuse', image: require('@/assets/partners/epiuse.png'), link: 'https://www.epiuse.com/' },
+        { name: 'maharatemp', image: require('@/assets/partners/maharatemp.png'), link: 'https://maharatemp.com/' },
+        { name: 'moharaa', image: require('@/assets/partners/moharaa.png'), link: 'https://moharaa.com/' },
+        { name: 'mwr', image: require('@/assets/partners/mwr.jpg'), link: 'https://mwrinfosecurity.com/' },
+        { name: 'onex', image: require('@/assets/partners/onexx.png'), link: 'https://onexyazilim.com/' },
+        { name: 'psyb', image: require('@/assets/partners/psyb.jpg'), link: 'https://psyb.co.za/' },
+        { name: 'retrorab', image: require('@/assets/partners/retrorab.png'), link: 'https://retrorabbit.co.za/' },
+        { name: 'synth', image: require('@/assets/partners/synth.png'), link: 'https://www.synth.co.za/' },
+        { name: 'ydar', image: require('@/assets/partners/ydar.jpg'), link: 'https://ydar.co.za/' },
+        { name: 'yello', image: require('@/assets/partners/yello.png'), link: 'https://yellow.co.za/' },
+        { name: 'zindi', image: require('@/assets/partners/zindi.png'), link: 'https://zindi.africa/' },
+        // // Add more partner objects as needed
+      ]
+    };
+  },
+
+
+
+  mounted() {
+    // Initialize Swiper
+    const swiper = new Swiper('.mySwiper', {
+      // Optional parameters
+      slidesPerView: this.getSlidesPerView(),
+      spaceBetween: 30,
+      // Navigation buttons
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      // Pagination
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+
+    // Update Swiper when window resizes
+    window.addEventListener('resize', () => {
+      swiper.params.slidesPerView = this.getSlidesPerView();
+      swiper.update();
+    });
+  },
+  methods: {
+    getSlidesPerView() {
+      if (window.innerWidth <= 788) {
+        return 1;
+      } else if (window.innerWidth <= 1215) {
+        return 2;
+      } else {
+        return 3;
+      }
+    },
+  }
+  };
+
+
+
+  
+
+
+
+ 
+
+   
+   
+  
+</script>
+  
+<style scoped>
+  #partners {
+  padding-top: 8.28rem;
+  padding-bottom: 8.28rem;
+
+    }
+
+
+    .sectionnn {
+  opacity: 0;
+  transform: translateY(5rem);
+  transition: opacity 0.5s ease, transform 0.5s ease;
+    }
+
+    .sectionnn.animate {
+  opacity: 1;
+  transform: translateY(0);
+    }
+    #parthead {
+  display: flex;
+  width: 100%;
+  height: 4.66667rem;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+  
+    }
+
+    #parthead h1 {
+  color: #5CAFF9;
+  text-align: center;
+  font-size: 2rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+    }
+
+
+    #swip,
+    .swiper-wrapper {
+  height: 21rem;
+  width: 100%;
+
+    }
+
+    .swiper-slide {
+  display: flex;
+  width: fit-content;
+  justify-content: center;
+  align-items: center;
+    }
+    .swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+    }
+
+
+
+
+
+    .swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+    }
+    .swiper-button-next,
+    .swiper-button-prev {
+  color: #5CAFF9;
+    }
+
+
+
+
+    #epiuspad {
+  padding: 3rem;
+    }
+
+    #awspad {
+  padding: 3.9rem 4.8rem;
+    }
+
+    #moharpad {
+  padding: 3rem 6rem;
+    }
+
+    #zinpadd {
+  padding: 0rem;
+    }
+
+    #darpad {
+  padding: 0rem 2rem;
+    }
+
+    #zinpadd {
+  padding: 1.5rem 0rem;
+    }
+
+    #bbdpad {
+  padding: 2.5rem;
+    }
+
+    #onexpadd {
+  padding: 3rem 6.5rem;
+    }
+
+    #mwrpad {
+  padding: 3.5rem;
+    }
+
+    #yellpad {
+  padding: 4rem 4.6rem;
+    }
+
+    #syntpad {
+  padding: 2.9rem 2rem;
+    }
+
+
+
+    #ydrpad {
+  padding: 1.5rem 3.8rem;
+    }
+
+
+
+    .partcontainer {
+  width: 12.11111rem;
+  height: 9.5rem;
+    }
+    .partcontainer img {
+  border-radius: 0.5rem;
+  
+    }
+    .partcontainer h1 {
+  font-size: 1.4rem;
+    }
+
+
+    .models {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  color: #FFF;
+  text-decoration: none;
+  bottom: -100%;
+  display: flex;
+  /* visibility: hidden; */
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 1.11111rem;
+  backdrop-filter: blur(0.2rem);
+  -webkit-backdrop-filter: blur(0.2rem);
+  background: linear-gradient(180deg, #17172900, #17172952, #5CAFF9);
+  transition: bottom 0.4s;
+    }
+
+
+    .partcontainer:hover .models {
+  bottom: 0;
+    }
+
+    .partcontainer div span {
+  display: flex;
+  flex-direction: row;
+  /* height: fit-content; */
+    }
+
+
+
+    .partcontainer div h4 {
+  margin-right: 0.4rem;
+  font-weight: 600;
+  font-size: 0.8rem;
+    }
+
+    .partcontainer:hover+.models {
+  visibility: visible;
+    }
+
+    .partcontainer img {
+  width: 13.66667rem;
+  height: 7.05556rem;
+  flex-shrink: 0;
+  /* mix-blend-mode: color-burn; */
+  /* filter: invert(1); */
+  object-fit: fill;
+  border-radius: 1.11111rem;
+  width: 100%;
+  height: 100%;
+    }
+    .viewsite {
+  width: 100%;
+  height: 1rem;
+  display: flex;
+  flex-direction: row;
+
+  justify-content: center;
+  align-items: center;
+    }
+
+
+
+</style>
+  
